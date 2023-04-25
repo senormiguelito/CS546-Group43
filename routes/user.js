b88f01ba675d827e41ebb758754af5c4db6d06ae
kaushal <85362707+kaushallearnscode@users.noreply.github.com>
2023-04-24 23:23:53 -0400

    some handlebars, and css file from my login signup code
    I took some base code from my assignment and started to push it, right now just design part so the whole code can work properly as it was instead of throwing errors, I will update it day by day from now on.
    
    IF STILL ANY ERROR OCCUR IN THE CODE I COMMITED, PLEASE COMMENT // THAT PART OF CODE AND WRITE ANY MESSAGE SO THAT IT WOULD BE EASIER FOR ME TO FIND AND SOLVE IT.

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,36 @@
 import { Router } from "express";
 const router = Router();
 
-router.get("/login", async (req, res) => {
+router.get("/user", async (req, res) => {
   try {
-    res.render("login", { title: "Login" });
+    res.render("home", { title: "Homepage" });
   } catch (e) {
-    res.sendStatus(500).redirect("/user");
+    res.sendStatus(500).redirect("/error");
   }
 });
+
+router
+  .get("/login", async (req, res) => {
+    try {
+      res.render("login", { title: "Login" });
+    } catch (e) {
+      res.sendStatus(500).redirect("/user");
+    }
+  })
+  .post(async (req, res) => {
+    //code here for POST
+  });
+
+router
+  .get("/signup", async (req, res) => {
+    try {
+      res.render("sign-up", { title: "Registration" });
+    } catch (e) {
+      res.sendStatus(500).redirect("/user");
+    }
+  })
+  .post(async (req, res) => {
+    //code here for POST
+  });
 
 export default router;
